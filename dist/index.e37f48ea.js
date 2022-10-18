99643c215c6864e832ce0c5b73442dd20b0164f1
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-10-18 23:01:14 +0200

    first API call

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -542,7 +542,29 @@ const timeout = function(s) {
 };
 // https://forkify-api.herokuapp.com/v2
 ///////////////////////////////////////
-console.log("test");
+const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=<insert your key>");
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        console.log(res, data);
+        let { recipe  } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.sourceUrl,
+            image: recipe.immage_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 },{}]},["fA0o9","aenu9"], "aenu9", "parcelRequire3a11")
 
